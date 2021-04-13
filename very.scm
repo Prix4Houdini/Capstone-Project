1d974b76b87da760266fea76f1941edc385eb901
Guru227 <narain2000@gmail.com>
2021-04-13 15:26:22 +0530

    implementation of lazy evaluation of regex in POSIX

--- very.scm
+++ very.scm
@@ -1,18 +1,72 @@
 #!/usr/local/bin/guile -s
 !#
-(newline)
-(define lis (command-line))
-(define arg1 (list-ref lis 1))
-(define arg2 (list-ref lis 2))
-(display arg1)(newline)
-(display arg2)(newline)
-(use-modules (ice-9 regex))
-(define file (open-input-file arg1))
+
 (use-modules (ice-9 textual-ports))
-(define text (get-string-all file))
-(define pattern "module[[:space:]]fa((.|\n)*)?endmodule")
-(define nom (map match:substring (list-matches pattern text)))
-(define first (list-ref nom 0))
-(display  first)
+(use-modules (ice-9 regex))
+
+(define (file-read)
+  ;; read command line arguments
+  (let ( (verilog-file (list-ref (command-line) 1)) )
+    ;; return file to calling proceedure
+    (
+     let ( (file (open-input-file verilog-file)) )
+      (get-string-all file)
+      )
+    )
+  )
+
+(define (regex-pattern-maker)
+    (let ((module-start "module[[:space:]]")
+	  (verilog-module (list-ref (command-line) 2))
+	  (body "((.|\n)*)?")
+	  (endmodule "endmodule"))
+		  
+      (string-append module-start verilog-module body endmodule))
+    )
+ 
+;;; implementation of lazy evaluation for regex matching
+;(define (regex-lazy-eval pattern source-text)
+  ;; apply pattern on source-text
+  ;; if patternized output is empty string, then return error
+  ;; else call regex-iter
+ ; )
+
+
+;;; core tail-recursive function of regex lazy evaluation
+(define (regex-iter pattern matched-text initial-text)
+  ;; DEFINITIONS OF LOCAL PROCEEDURES
+  ;; removes the characters 'endmodule' from the end of the text using indices to slice the string
+  (define (remove-endmodule)
+    (define new_len (- (string-length matched-text) 9))
+    (substring matched-text 0 new_len)
+    )
+  ;; proceedure to match regex with text
+  (define (match-regex t-string)
+    (define match-list (map match:substring (list-matches pattern t-string)))
+    (if (= (length match-list) 0) "" (list-ref match-list 0))
+    )
+
+  ;; PROCEEDURE
+  ;; if matched-text is an empty string
+  (if (string= "" matched-text)
+      initial-text   ; if condition is true - termination condition
+      
+      ;; else call regex-iter once again
+      (let ( (temp-string (match-regex (remove-endmodule))))
+	(regex-iter pattern temp-string matched-text) ; call regex-iter tail-recursively
+      ))
+  )
+ 
+(define temp (file-read))
+(define tepm (regex-pattern-maker))
+
+
+
 (newline)
-;((?!endmodule).)*
+
+
+
+
+
+
+
