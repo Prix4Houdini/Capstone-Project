283756e13cc70786dd3c5dd553bb1f29473e8e78
Guru227 <narain2000@gmail.com>
2021-05-07 13:15:58 +0530

    refactored module extractor, added comments

--- module-extractor.scm
+++ module-extractor.scm
@@ -3,53 +3,70 @@
 
 (use-modules (ice-9 textual-ports))
 (use-modules (ice-9 regex))
-;;(use-modules (comment-reducer))
 
+;; MODULE EXTRACTOR
 (define (file-read)
-  (let ( (verilog-file (list-ref (command-line) 1)) ) ; read verilog filename from command line arg
-    (
-     let ( (file (open-input-file verilog-file)) ) ; open file-handler
-      (get-string-all file) ; read file
-      )
-    )
-  )
+  (let* ((file-name    (list-ref (command-line) 1))  ; read verilog filename from command line arg
+	 (file-handler (open-input-file file-name))) ; open file-handler
+    (get-string-all file-handler))) ; read file  
 
-(define (regex-pattern-maker)
-  ;; syntax of module definition
+(define (module-begin-pattern) ; create regex pattern for module beginning
     (let ((module-start "module[[:space:]]") 
-	  (verilog-module (list-ref (command-line) 2))
-	  (body "((.|\n)*)?")
-	  (endmodule "endmodule"))
-		  
-      (string-append module-start verilog-module body endmodule)) ; create complete regex
-    )
- 
-;;; implementation of lazy evaluation for regex matching
-(define (regex-lazy-eval pattern source-text)
+	  (verilog-module (list-ref (command-line) 2)))	  
+      (string-append module-start verilog-module))) 
+
+(define (extractor pattern text) ; extract module from text file
+  (define matched-string (string-match pattern text)) ; returns match object, else returns false
+  (if (eq? #f matched-string)
+      ""
+      (let* ((st   (match:start matched-string)) ; get starting index from match object
+	     (end (match:end   (string-match "endmodule" text st)))) ; index of endmodule
+	(substring text st end)))) ; return substring
+    
+;; COMMENT ELIMINATOR
+(define (find-comment-range text st-index)
+  ;;PROCEDURE DEFINITIONS
+  (define (find-slash st-idx)
+    (string-index text #\/ st-idx))
+  (define (next-asterisk? st-idx)
+    (string= "*" (substring text (+ st-idx 1) (+ st-idx 2))))
+  (define (prev-asterisk? st-idx)
+    (string= "*" (substring text (- st-idx 1) st-idx)))
   
-  ;;PROCEDURE
-  (let ( (temp-string (match-regex pattern source-text)))
-    (if (string= "" temp-string)
-	"" ; if module does not exist, return empty string
-	(let* ( (intermediate-output (string-match "endmodule" temp-string))
-		(last-index (match:end intermediate-output))
-		)
-	  (substring temp-string 0 last-index))
-	))
-  )
+  ;;PROCEDURE   
+  (define a (find-slash st-index))  
+  (cond ((equal? #f a) a) ; return #f if there are no more comments
+	((next-asterisk? a) ; if asterisk follows forward slash - multiline comments
+	 (let ((temp (find-slash (+ a 1)))) ; find next slash
+	   (cond ((prev-asterisk? temp)
+		  (list a (+ temp 1)))))) ; return a list (a (+ temp 1))
+					  ; haven't handled condition where previous is not asterisk
+	(else (list a (string-index text #\newline a))))) ; single line comments
+		   
+(define (remove-comments-iter op_text ip_text st)
+  ;; PROCEDURE DEFINITIONS
+  (define (extract-till end)
+    (substring ip_text st end))
+  (define (extract-till-end)
+    (substring ip_text st))
+  
+  ;; get start and end indices of comment
+  (define char-range (find-comment-range ip_text st))
+  (if (equal? #f char-range)
+      (string-append op_text (extract-till-end)) ; return till the end of string if last comment
+      (let ((temp (string-append op_text (extract-till (list-ref char-range 0)))))
+	(remove-comments-iter temp ip_text (list-ref char-range 1))))) ; remove next comment
 
-;; proceedure to match regex with text
-(define (match-regex pattern t-string)
-  (define match-list (map match:substring (list-matches pattern t-string)))
-  (if (= (length match-list) 0) "" (list-ref match-list 0))
-  )
 
+;; WRAPPER
+(define (module-extractor)
+  (let* ((ip  (file-read))
+	(pat (module-begin-pattern))
+	(int (extractor pat ip)))
+    (remove-comments-iter "" int 0)))
 
 ;; MAIN 
-(define text (file-read))
-(define r-pat (regex-pattern-maker))
-(define ext-module (regex-lazy-eval r-pat text))
-(display ext-module)
+(display (module-extractor))
 
 (newline)
 
