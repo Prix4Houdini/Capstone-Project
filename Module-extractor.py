b594f1435fb5ff4955523973897118b2c9e10048
Prix4Houdini <romaanchanskanda@gmail.com>
2021-12-07 15:22:19 +0530

    Module extractor and Tree detection Function Updates

--- Module-extractor.py
+++ Module-extractor.py
@@ -1,28 +1,27 @@
 import sys
 import re
 
-l = []
-a = sys.argv[1]
-opener = open(a)
-string = opener.read()
-opener.close()
-te = re.findall("module .*\(", string)
-for i in te:
-    temp = i.find(' ')
-    l.append(i[temp+1:-1])
-
-final_module_definitions = []
-for module_name in l:
-    pattern_search = re.search(f"module\s{module_name}\(((?!endmodule).|\s)*endmodule", string)
-    final_module_definitions.append(string[pattern_search.start():pattern_search.end()])
 
 
-output_file = open("D:\Curiculum\Sem 7\Capstone Project\Capstone-Project\out.v", 'w')
-#output_file.close()
+def module_extractor(input_string):
+    te = re.findall("module .*\(", input_string)
+    for i in te:
+        temp = i.find(' ')
+        l.append(i[temp + 1:-1])
+    final_module_definitions = []
+    for module_name in l:
+        pattern_search = re.search(f"module\s{module_name}\(((?!endmodule).|\s)*endmodule", string)
+        final_module_definitions.append(string[pattern_search.start():pattern_search.end()])
 
-for module in final_module_definitions:
-    output_file.write(module + '\n\n')
-output_file.close()
+    return final_module_definitions
 
-tree_detection = re.findall("tree\(.*\);", string)
-print(tree_detection)
+def tree_extractor(input_string):
+    tree_detection = re.findall("tree\(.*\);", string)
+    return tree_detection
+
+
+def module_output(final_module_definitions, file_output):
+    opener = open(file_output, 'w')
+    for module in final_module_definitions:
+        opener.write(module + '\n\n')
+    opener.close()
