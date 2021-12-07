9e1f6bc3dfda4a91fa4c46c6921b29472c0a109d
Prix4Houdini <romaanchanskanda@gmail.com>
2021-12-07 15:23:44 +0530

    Module extractor and Tree detection Function Updates

--- Module-extractor.py
+++ Module-extractor.py
@@ -4,19 +4,20 @@ import re
 
 
 def module_extractor(input_string):
+    l = []
     te = re.findall("module .*\(", input_string)
     for i in te:
         temp = i.find(' ')
         l.append(i[temp + 1:-1])
     final_module_definitions = []
     for module_name in l:
-        pattern_search = re.search(f"module\s{module_name}\(((?!endmodule).|\s)*endmodule", string)
-        final_module_definitions.append(string[pattern_search.start():pattern_search.end()])
+        pattern_search = re.search(f"module\s{module_name}\(((?!endmodule).|\s)*endmodule", input_string)
+        final_module_definitions.append(input_string[pattern_search.start():pattern_search.end()])
 
     return final_module_definitions
 
 def tree_extractor(input_string):
-    tree_detection = re.findall("tree\(.*\);", string)
+    tree_detection = re.findall("tree\(.*\);", input_string)
     return tree_detection
 
 
